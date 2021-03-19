f50c4fc3dd11a1768c6e395df57ff1ddad35e7ee
Student <student@g006-04.rz.fh-ingolstadt.de>
2021-03-19 15:16:49 +0100

    roll a ball finished

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # VR-Praktikum-THI
 #first edit to control commit
 #changed folder to Project
+
+#Roll a ball:
+#dieses Projekt enthält eine funktionierende Version mit VR-Unterstützung für "Roll A Ball". 
+#Ball rollt
+#Spielfeld halt Borders
+#10 gelbe, rotierende Cubes sind über die Spielfläche verteilt
+#bei Collision mit den Pickups verschwinden diese
+
+
---
+++ Roll a ball/Assets/Skripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public bool keyboard;
+    public float speed;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        if(keyboard)
+        {
+            float moveHorizontal = Input.GetAxis ("Horizontal"); 
+            float moveVertical = Input.GetAxis ("Vertical");
+            movement = new Vector3(moveHorizontal,0f,moveVertical);
+            movement=movement *speed;
+            rb.AddForce(movement);
+        }
+        
+    }
+    void OnTriggerEnter(Collider other) 
+    { 
+        if (other.gameObject.CompareTag ("Pick Up")) 
+        { 
+            other.gameObject.SetActive (false); 
+        } 
+    }
+}
