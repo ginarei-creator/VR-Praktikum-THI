bfd31b91b59976d7c5a90746fd35e132058ce58f
Student <student@g006-04.rz.fh-ingolstadt.de>
2021-03-19 16:57:50 +0100

    roll a ball finished and score computed

--- Roll a ball/Assets/TestText.cs
+++ Roll a ball/Assets/TestText.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//public class TestText : MonoBehaviour
-//{
-   // public Text scoreText;  // public if you want to drag your text object in there manually
-    //int scoreCounter;
-    //public GameObject player;
+public class TestText : MonoBehaviour
+{
+    public Text scoreText;  // public if you want to drag your text object in there manually
+    int scoreCounter;
+    public GameObject player;
     // Start is called before the first frame update
-    //void Start()
-    //{
-   //     scoreText = GetComponent<Text>();
-    //}
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
 
     // Update is called once per frame
-    //void Update()
-   // {
-    //    scoreCounter = PlayerControl.score;
-    //   scoreText.text = scoreCounter.ToString();
-    //}
-//}
+    void Update()
+    {
+        scoreCounter = player.score;
+       scoreText.text = scoreCounter.ToString();
+    }
+}
