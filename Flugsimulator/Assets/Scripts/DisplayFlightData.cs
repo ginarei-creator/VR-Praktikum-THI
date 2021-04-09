6278c9b0bcbd09be869ff235d22d477af6b99c66
Student <student@g006-04.rz.fh-ingolstadt.de>
2021-04-09 11:34:37 +0200

    modifying DisplayFlightData

---
+++ Flugsimulator/Assets/Scripts/DisplayFlightData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayFlightData : MonoBehaviour
+{
+    public Text textSpeed;
+    public Text textHeight;
+    public GameObject player;
+    public float warningHeight;
+    public GameObject warnImage;
+    private FlyJoystick flyjoystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyJoystick = player.GetComponent <FlyJoystick >();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Flugsimulator/Assets/Scripts/FlyJoystick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyJoystick : MonoBehaviour
+{
+    public float speed;
+    public float speedAmplifier;
+    public float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputPitch = Input.GetAxis("Pitch");
+        float inputRoll = Input.GetAxis("Roll");
+        float inputYawn = Input.GetAxis("Yawn");
+        float inputThrottle = Input.GetAxis("Throttle");
+        Vector3 rotationMovement = new Vector3 (inputPitch * Time.deltaTime * rotationSpeed, inputYawn * Time.deltaTime * rotationSpeed, inputRoll * Time.deltaTime * rotationSpeed );
+        this.transform.Rotate(rotationMovement);
+        speed = inputThrottle * speedAmplifier;
+        this.transform.Translate (Vector3.forward * speed * Time.deltaTime);
+    }
+}
